7db260ba1ca85dee750ed4db1540bdee794fbeec
ayodeji oni <ayodejioni1505@gmail.com>
2024-11-04 12:38:41 +0100

    Included more screens, code for testing turbidity sensor

--- raspberry-config/sensors/turbidity.py
+++ raspberry-config/sensors/turbidity.py
@@ -24,44 +24,44 @@ Interfacing Protocol: SPI (Serial Peripheral Interface)
 **Requires ADC(MCP3008) -> 10-bit 
 """
 
-def turbidity_post(args):
-    '''
-    Meant to perform Power On Self Test (POST)
-    '''
-    pass
+# def turbidity_post(args):
+#     '''
+#     Meant to perform Power On Self Test (POST)
+#     '''
+#     pass
 
 
-def turbidity(args):
-    pass
+# def turbidity(args):
+#     pass
 
 
 
 
-# import spidev
-# import time
+import spidev
+import time
 
-# # SPI setup
-# spi = spidev.SpiDev()
-# spi.open(0, 0)  # Open on bus 0, device 0
-# spi.max_speed_hz = 1350000
+# SPI setup
+spi = spidev.SpiDev()
+spi.open(0, 0)  # Open on bus 0, device 0
+spi.max_speed_hz = 1350000
 
-# def read_adc(channel):
-#     adc = spi.xfer2([1, (8 + channel) << 4, 0])
-#     data = ((adc[1] & 3) << 8) + adc[2]
-#     return data
+def read_adc(channel):
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])
+    data = ((adc[1] & 3) << 8) + adc[2]
+    return data
 
-# def calculate_voltage(adc_value):
-#     return adc_value * (3.3 / 1023.0)  # For 3.3V reference
+def calculate_voltage(adc_value):
+    return adc_value * (3.3 / 1023.0)  # For 3.3V reference
 
-# def calculate_turbidity(voltage):
-#     # Example linear approximation
-#     # You may need to adjust these based on calibration data
-#     return 3000 - (voltage * 1000)
+def calculate_turbidity(voltage):
+    # Example linear approximation
+    # You may need to adjust these based on calibration data
+    return 3000 - (voltage * 1000)
 
-# while True:
-#     adc_value = read_adc(0)  # Channel 0 for turbidity sensor
-#     voltage = calculate_voltage(adc_value)
-#     turbidity = calculate_turbidity(voltage)
+while True:
+    adc_value = read_adc(0)  # Channel 0 for turbidity sensor
+    voltage = calculate_voltage(adc_value)
+    turbidity = calculate_turbidity(voltage)
     
-#     print(f"Turbidity: {turbidity} NTU")
-#     time.sleep(1)
+    print(f"Turbidity: {turbidity} NTU")
+    time.sleep(1)
