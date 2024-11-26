f42a513afd228a4860af54fdb55368376e1a08aa
ayodeji oni <ayodejioni1505@gmail.com>
2024-11-26 11:36:57 +0100

    Data exchange completed in firestore test mode

--- raspberry-config/firestore.py
+++ raspberry-config/firestore.py
@@ -1,38 +1,41 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Initialize Firebase
-cred = credentials.Certificate("firebase-services-account.json")
-firebase_admin.initialize_app(cred)
+try:
+    # Initialize Firebase
+    cred = credentials.Certificate("firebase-services-account.json")
+    firebase_admin.initialize_app(cred)
 
-# Access Firestore
-db = firestore.client()
+    # Accessing Firestore
+    db = firestore.client()
 
-# Update data for water quality parameters
-water_parameters = db.collection("waterParameters").document("ko9ATHykCZnw3RxkVzpP")
-water_parameters.update({
-    "do": 15,
-    "ec": 600,
-    "hardness": 250,
-    "ph": 7.68,
-    "salinity": 18,
-    "temperature": 25,
-    "turbidity": 3,
-    "wqi": 23.9,
-})
+    # Update data for water quality parameters
+    water_parameters = db.collection("waterParameters").document("ko9ATHykCZnw3RxkVzpP")
+    water_parameters.update({
+        "do": 15,
+        "ec": 600,
+        "hardness": 250,
+        "ph": 7.68,
+        "salinity": 18,
+        "temperature": 25,
+        "turbidity": 3,
+        "wqi": 23.9,
+    })
 
-pump_control = db.collection("pumpControl").document("fCgyfcht2wPkn1TJ05KE")
-pump_control.update({
-    "water_level": 70.34,
-})
+    # Update data for Water Level
+    pump_control = db.collection("pumpControl").document("fCgyfcht2wPkn1TJ05KE")
+    pump_control.update({
+        "water_level": 70.34,
+    })
 
-# Read data
-doc = pump_control.get()
-if doc.exists:
-    doc = doc.to_dict()
-    print(f"Document data: {doc['mode'], doc['switch']}")
-    print(doc)
-else:
-    print("No such document!")
-
+    # Read pump control data
+    doc = pump_control.get()
+    if doc.exists:
+        doc = doc.to_dict()
+        print(f"Document data: {doc['mode'], doc['switch']}")
+        print(doc)
+    else:
+        print("No such document!")
+except Exception as e:
+    print(f"Error: {e}")
 
