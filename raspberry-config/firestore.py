47676081e04c7ed1bb481a0ba048efcb77e6b73c
ayodeji oni <ayodejioni1505@gmail.com>
2024-11-26 11:21:37 +0100

    Data exchange completed in firestore test mode

--- raspberry-config/firestore.py
+++ raspberry-config/firestore.py
@@ -1,36 +1,40 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Initialize Firebase
-cred = credentials.Certificate("firebase-services-account.json")
-firebase_admin.initialize_app(cred)
+try:
+    # Initialize Firebase
+    cred = credentials.Certificate("firebase-services-account.json")
+    firebase_admin.initialize_app(cred)
 
-# Access Firestore
-db = firestore.client()
+    # Access Firestore
+    db = firestore.client()
 
-# Update data for water quality parameters
-water_parameters = db.collection("waterParameters").document("ko9ATHykCZnw3RxkVzpP")
-water_parameters.update({
-    "do": 15,
-    "ec": 600,
-    "hardness": 250,
-    "ph": 7.68,
-    "salinity": 18,
-    "temperature": 25,
-    "turbidity": 3,
-    "wqi": 23.9,
-})
+    # Update water quality parameters
+    water_parameters_ref = db.collection("waterParameters").document("ko9ATHykCZnw3RxkVzpP")
+    water_parameters_ref.update({
+        "do": 15,
+        "ec": 600,
+        "hardness": 250,
+        "ph": 7.68,
+        "salinity": 18,
+        "temperature": 25,
+        "turbidity": 3,
+        "wqi": 23.9,
+    })
 
-pump_control = db.collection("pumpControl").document("fCgyfcht2wPkn1TJ05KE")
-pump_control.update({
-    "water level": 70.34,
-})
+    # Update pump control parameters
+    pump_control_ref = db.collection("pumpControl").document("fCgyfcht2wPkn1TJ05KE")
+    pump_control_ref.update({
+        "water level": 70.34,
+    })
 
-# Read data
-doc = pump_control.collection("pumpControl").document("fCgyfcht2wPkn1TJ05KE").get()
-if doc.exists:
-    doc = doc.to_dict()
-    # print(f"Document data: {doc['mode'], doc['switch']}")
-    print(doc)
-else:
-    print("No such document!")
+    # Read pump control data
+    doc = pump_control_ref.get()
+    if doc.exists:
+        pump_control_data = doc.to_dict()
+        print(f"Document data: {pump_control_data['mode'], pump_control_data['switch']}")
+    else:
+        print("No such document!")
+
+except Exception as e:
+    print(f"An error occurred: {e}")
